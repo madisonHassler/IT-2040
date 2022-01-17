0ad8fdd0542fe02ebe6ed561fe685b77d14c4b2d
Madison Hassler <mehfmd@umsystem.edu>
2022-01-16 22:11:32 -0600

    comments

--- FizzBuzz.c
+++ FizzBuzz.c
@@ -4,18 +4,18 @@
  
      for (int i=1; i<=100; i++){
  
-         if ((i % 3) == 0 && (i % 5) == 0){
+         if ((i % 3) == 0 && (i % 5) == 0){ /*test if divisible by 3 and 5*/
              printf("FizzBuzz\n");
          }
          else{
-             if ((i % 5) == 0){
+             if ((i % 5) == 0){ /*if not divisible by 3 and 5, test if divisible by only 5*/
                  printf("Bizz\n");
              }
              else{
-                 if ((i % 3) == 0){
+                 if ((i % 3) == 0){ /*if not divisible by 5, test if divisible by only 3*/
                      printf("Fizz\n");
                  }
-                 else{
+                 else{ /*if not divisible by (3 & 5), 5, or 3, print integer*/
                      printf("%d\n", i);
                  }
              }
